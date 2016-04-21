18e1a5f8c01e4f893e3461d46f7785737ec4820a
robinkson <r-gomez-12@hotmail.com>
2016-04-20 20:57:45 -0500

    "completando el metodo listar sede"
    ahora solo falta el metodo de borrar y consultar no?

--- src/controlador/Controlador_Sede.java
+++ src/controlador/Controlador_Sede.java
@@ -20,6 +20,34 @@ public class Controlador_Sede {
         //salonABuscar.mostrarDatos();    
     } //los resultados se ven en la BD
     
-    
+    public String[][] ListarTodasLasSedes(){
+        
+        List<SedeDTO> lstSd; //necesitare la lista de Sedes...
+        SedeDTO sd; //tambien un objeto sede independiente para trasformarlo...
+        String[][] datos; //por que voy a necesitar mandar esa Matriz de Strings para que la Tabla la Pinte.
+        
+        SedeDAO s = new SedeDAO(); //creo una nueva instancia para el movimiento de los datos en la BD.
+        lstSd = s.readAll(); // Le paso toda la lista de SedesDTO a la variable "lstSd";
+        
+        int numFilas = lstSd.size(); //extraigo el numero de Filas desde el tamaño del arreglo... (en minusculas por que no es una constante si no que su valor varia dependiendo del numero de regitros que halla en el arreglo)
+        final int NUM_COLUMNAS = 3; //por que ya se que son las columnas: codigo, nombre, ubicacion;
+        
+        datos = new String[numFilas][NUM_COLUMNAS];
+        
+        for (int i = 0; i < (datos.length); i++) {
+            String[] filaTemporalSede = {"","",""};
+            //for (int j = 0; j < (datos[i].length); j++) {
+                 
+                 sd = lstSd.get(i); //paseme el objeto SedeDTO que este en esa posicion i;
+                 filaTemporalSede[0] = sd.getCodigo(); //llene la posicion 0 de la fila temporal con el codigo
+                 filaTemporalSede[1] = sd.getNombre(); //llene la posicion 0 de la fila temporal con el nombre
+                 filaTemporalSede[2] = sd.getDireccion(); //llene la posicion 0 de la fila temporal con la direccion
+                
+            //}
+            datos[i] = filaTemporalSede;
+        }
+        return datos;
+        //los resultados se ven en la Aplicacion
+    }
     
 }
