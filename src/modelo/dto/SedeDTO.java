89debd7e3fd1fb86d0adc8e0faf06dac715ee1a6
robinkson <r-gomez-12@hotmail.com>
2016-04-20 22:32:58 -0500

    Aqui estoy agregando de apocos el SedeDTO chicos
    Me faltan un par de metodos ya lo adiciono...

--- src/modelo/dto/SedeDTO.java
+++ src/modelo/dto/SedeDTO.java
@@ -5,8 +5,18 @@ package dto;
  * @author
  */
 public class SedeDTO {
+    //declarando las variables
+    private String codigo, nombre, direccion;
     
-    
+    //sobrecarga de constructores...
+
+    //sobrecarga de constructores...
+    public SedeDTO() {}
+    public SedeDTO(String codigo) {this.codigo = codigo;}
+    public SedeDTO(String codigo, String nombre, String direccion) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.direccion = direccion;
     }
     
 }
