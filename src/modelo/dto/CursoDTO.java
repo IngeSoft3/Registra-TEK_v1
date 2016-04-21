85b9ec22a7ac10211f4f41c5063c687540e7ef34
Jeyned <Jeyned@hotmail.com>
2016-04-21 03:54:55 -0500

    Declarando el Método que muestra los datos...

--- src/modelo/dto/CursoDTO.java
+++ src/modelo/dto/CursoDTO.java
@@ -20,7 +20,6 @@ public class CursoDTO {
         this.sede_codigo = sede_codigo;
     }
     
-    
     //metodos de acesso SET...
     public void setCodigo(String codigo) {this.codigo = codigo;}
     public void setNombre(String nombre) {this.nombre = nombre;}
@@ -33,5 +32,16 @@ public class CursoDTO {
     public String getJornada() {return jornada;}
     public int getSede_codigo() {return sede_codigo;}
     
-    
+    //Metodos Operacionales...
+    /* No implemente mostrarDatos() con toString() con override por que cada  
+     * que lo hacia no me mostraba los datos del objeto si no que antes me
+     * mostraba era el tipo de dato mas la posicion en memoria. 
+     */
+    public void mostrarDatos() { //metodo para mostrar los datos de este objeto
+        System.out.println (" Curso: \n------------------- " + 
+             "\n codigo  = \t" + codigo + 
+             "\n nombre  = \t" + nombre + 
+             "\n jornada = \t" + jornada + 
+             "\n-------------------"); 
+    }
 }
