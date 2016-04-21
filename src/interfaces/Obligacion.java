97ec4aa1b1556affa8cbfb6d8e63e01aeb03abb2
EmmanuelBarreraMarin <emmanuelbm1128@gmail.com>
2016-04-21 03:10:41 -0500

    Aqui termino interface...
    Si muchachos estandaricemos esto coloquemos le interface para manejar el código mismo...

--- src/interfaces/Obligacion.java
+++ src/interfaces/Obligacion.java
@@ -7,7 +7,13 @@ import java.util.List;
  * @author
  */
 public interface Obligacion <Cualquiercosa> {
-
+    
+    //metodos de manejo de la Base de datos...
+    public boolean create(Cualquiercosa c);
+    public boolean delete(Object key);
+    public boolean update(Cualquiercosa c);
+    
+    public Cualquiercosa read(Object key);
     public List<Cualquiercosa> readAll();
     
 }
