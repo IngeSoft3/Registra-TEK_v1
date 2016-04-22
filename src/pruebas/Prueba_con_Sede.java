57ba8d62009b7019c73c6d9ba93f8700ae2570d5
Leonardo Collazos Paez <leo011093@hotmail.com>
2016-04-22 07:04:14 -0500

    Refinando pruebas de unidad con sede...

--- src/pruebas/Prueba_con_Sede.java
+++ src/pruebas/Prueba_con_Sede.java
@@ -11,12 +11,40 @@ import dto.SedeDTO;
 public class Prueba_con_Sede {
     
     public static void main(String[] args) {
-        SedeDAO s = new SedeDAO();
+        /*SedeDAO s = new SedeDAO();
         s.create(new SedeDTO("123", "sede1", "desconocida"));
         SedeDTO salonABuscar = s.read("1100");
         System.out.println(salonABuscar.toString());
         s.update(new SedeDTO("222", "salon1", "unknown"));
-        salonABuscar.mostrarDatos();
+        salonABuscar.mostrarDatos();*/
+        
+        prueba_CRUD();
+        //prueba_ListarSedeConCodigo();
+        
+    }
+    
+    public static void prueba_CRUD() {
+        
+        //declarando los objetos que utilizare para la prueba
+        SedeDTO sdto; 
+        SedeDAO sdao;
+        
+        sdao = new SedeDAO();
+        sdao.create(new SedeDTO("123", "sede1", "desconocida"));
+        System.out.println("Creacion Satisfactoria \n");
+        
+        sdto = sdao.read("123");
+        System.out.println("objeto salon: " + sdto.toString() + " almacenado en memoria");
+        sdto.mostrarDatos();
+        System.out.println("Consulta Satisfactoria \n");
+        
+        sdao.update(new SedeDTO("123", "sedeMod", "unknown"));
+        sdto = sdao.read("123"); //lo busco de nuevo para mostrar sus cambios en la consola
+        sdto.mostrarDatos(); 
+        System.out.println("Modificacion Satisfactoria \n");
+        
+        sdao.delete("123");
+        System.out.println("Eliminacion Satisfactoria \n");
     }
     
     public static void prueba_ListarSedeConCodigo() {
