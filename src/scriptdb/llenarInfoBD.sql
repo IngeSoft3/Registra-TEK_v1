9c8acea7b2b4dfec77648b25bef6dd99ef3e0dac
jlnnarvaez <jlnnarvaez@gmail.com>
2016-04-22 07:11:34 -0500

    Población de la Base de Datos en tabla Sede...

---
+++ src/scriptdb/llenarInfoBD.sql
@@ -0,0 +1,14 @@
+
+-- Este script me llena la Informacion de la BD, con la 
+-- info principal, de la sede, cursos, materias, etc...
+
+-- -----------------------------------------------------
+-- Table rg_tk.sede
+-- -----------------------------------------------------
+INSERT INTO sede (codigo, nombre, direccion) VALUES (1100, "principal", "norte");
+INSERT INTO sede (codigo, nombre, direccion) VALUES (2200, "secundaria", "noroeste");
+INSERT INTO sede (codigo, nombre, direccion) VALUES (3300, "terciaria", "Sur");
+INSERT INTO sede (codigo, nombre, direccion) VALUES (4400, "4to-level", "oriente");
+INSERT INTO sede (codigo, nombre, direccion) VALUES (5500, "5to-level", "Occidente");
+
+-- -----------------------------------------------------
