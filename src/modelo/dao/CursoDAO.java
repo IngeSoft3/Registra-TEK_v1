0cc6aceee59865c4aac89dfa7a748c44c0451dbf
Leonardo Collazos Paez <leo011093@hotmail.com>
2016-04-22 00:04:35 -0500

    correccion SQL y agregando metodo DELETE ...

--- src/modelo/dao/CursoDAO.java
+++ src/modelo/dao/CursoDAO.java
@@ -12,7 +12,7 @@ import interfaces.Obligacion;
 public class CursoDAO implements Obligacion<CursoDTO>{
     
     //constantes que me permitiran manejar las sentencias SQL de la base de datos
-    private static final String  SQL_INSERT = "INSERT INTO curso (codigo, nombre, jornada, sede_codigo) VALUES (?, ?, ?, ?)"";
+    private static final String  SQL_INSERT = "INSERT INTO curso (codigo, nombre, jornada, sede_codigo) VALUES (?, ?, ?, ?)";
     private static final String  SQL_DELETE = "DELETE FROM curso WHERE codigo = ?";
     private static final String  SQL_UPDATE = "UPDATE salon SET nombre=?, jornada=?, sede_codigo=? WHERE codigo = ?";
     private static final String    SQL_READ = "SELECT * FROM curso WHERE codigo = ?";
@@ -29,7 +29,21 @@ public class CursoDAO implements Obligacion<CursoDTO>{
 
     @Override
     public boolean delete(Object key) {
-         
+        PreparedStatement ps;
+        try {
+            ps = con.getCnn().prepareStatement(SQL_DELETE);
+            ps.setString(1, key.toString());
+            
+            if (ps.executeUpdate() > 0){
+                // con.cerrarConexion(); no estaba, pero me aseguro por si las...
+                return true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CursoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            con.cerrarConexion(); 
+        }
+        return false; 
     }
 
     @Override
