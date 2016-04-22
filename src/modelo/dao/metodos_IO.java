e4dd14a5c7496a24d1cd319430efb268063aae79
robinkson <r-gomez-12@hotmail.com>
2016-04-22 00:31:45 -0500

    métodos simplificados para leer Números Enteros...

--- src/modelo/dao/metodos_IO.java
+++ src/modelo/dao/metodos_IO.java
@@ -20,4 +20,26 @@ public class metodos_IO {
         JOptionPane.showMessageDialog(null, mensaje); 
     }
     
+    /**
+     * //método para leer un dato de tipo string y convertirlo a entero
+     * @param mensaje
+     * @return 
+     */
+    public static int leerInt(String mensaje) {
+        String N = JOptionPane.showInputDialog(mensaje);
+        int n = Integer.parseInt(N);
+        return n;
+    }
+    
+    /**
+     * método para leer un dato string y convertirlo a Entero.
+     * @param dato
+     * @return 
+     */
+    public static int leerIntSinMensaje(String dato) { //
+        int n = Integer.parseInt(dato);
+        return n;
+    }
+    
+    
 }
