8f9a7f1eaa9e5d4e0d39a6af57c319dcca5379d2
EmmanuelBarreraMarin <emmanuelbm1128@gmail.com>
2016-04-22 08:07:14 -0500

    creando Método que lista todos los Estudiantes...

--- src/controlador/Controlador_Estudiante.java
+++ src/controlador/Controlador_Estudiante.java
@@ -43,4 +43,36 @@ public class Controlador_Estudiante {
     return FilaEstudiante;
   } //los resultados se ven en la Aplicacion
   
+  public String[][] ListarTodasLasEstudiantes(){
+        
+    List<EstudianteDTO> lstEst; //necesitare la lista de Estudiantes...
+    EstudianteDTO ed; //tambien un objeto sede independiente para trasformarlo...
+    String[][] datos; //por que voy a necesitar mandar esa Matriz de Strings para que la Tabla la Pinte.
+    
+    EstudianteDAO e = new EstudianteDAO(); //creo una nueva instancia para el movimiento de los datos en la BD.
+    lstEst = e.readAll(); // Le paso toda la lista de EstudiantesDTO a la variable "lstDct";
+    
+    int numFilas = lstEst.size(); //extraigo el numero de Filas desde el tamaño del arreglo... (en minusculas por que no es una constante si no que su valor varia dependiendo del numero de regitros que halla en el arreglo)
+    final int NUM_COLUMNAS = 3; //por que ya se que son las columnas: codigo, nombre, ubicacion;
+    
+    datos = new String[numFilas][NUM_COLUMNAS];
+    
+    for (int i = 0; i < (datos.length); i++) {
+      String[] filaTemporalEstudiante = {"","","","",""};
+    //for (int j = 0; j < (datos[i].length); j++) {
+     
+        ed = lstEst.get(i); //paseme el objeto EstudianteDTO que este en esa posicion i;
+        filaTemporalEstudiante[0] = ed.getCodigo(); //llene la posicion 0 de la fila temporal con el codigo
+        filaTemporalEstudiante[1] = ed.getNombre(); //llene la posicion 0 de la fila temporal con el nombre
+        filaTemporalEstudiante[2] = ed.getApellidos(); //llene la posicion 0 de la fila temporal con la direccion
+        filaTemporalEstudiante[3] = ed.getFechaNacimiento()+""; //llene la posicion 0 de la fila temporal con la direccion
+        filaTemporalEstudiante[4] = ed.getCodigo_curso()+""; //llene la posicion 0 de la fila temporal con la direccion
+      
+    //}
+      datos[i] = filaTemporalEstudiante;
+    }
+    return datos;
+    //los resultados se ven en la Aplicacion
+  }
+  
 }
